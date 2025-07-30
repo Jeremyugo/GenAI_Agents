93a95aa411e0cc6988043e194c55d542b6b4b749
Chinyelugo Jeremiah <36512525+Jeremyugo@users.noreply.github.com>
2025-07-30 14:54:48 -0600

    tested new Agentic workflow

--- self-corrective-agentic-RAG/README.md
+++ self-corrective-agentic-RAG/README.md
@@ -1,14 +1,32 @@
-# Corrective x Agentic RAG 🤖
+# Agentic Self-Corrective RAG with Hybrid Vector Store & Knowledge Graph 🤖
 
-This directory contains code and demo of a self corrective and agentic RAG capable of:
-1. Self-reflection
-2. Self-grading
-3. Reranking
-3. Self-control (decides whether to use retriever tool or search the web for more information)
+This project is an advanced information retrieval and generation system (RAG) designed to produce more accurate, 
+reliable answers by combining semantic information from a vector store, relationships from a knowledge graph and self
+correcting logic. 
+Traditional RAG (Retrieval-Augmented Generation) systems rely mainly on a vector store, which retrieves content based on 
+semantic similarity to a user's query. However, vector stores alone often miss relationships between data. 
 
-## TODO:
-- [ ] Add query classifier
-- [x] Build a Knowledge Graph
+**This project improves on that by adding:**
+- Agents that guide the system, evaluate results, and decide next steps. 
+- A knowledge graph, which understands the relationship between data points. 
+- A feedback loop, where the system checks and corrects its own results. 
+- A web search tool, which searches for additional information if required. 
+
+This creates a smarter system that retrieves better information and generates more accurate responses. 
+
+**Key Features:**
+- Classifies User query into either 'Conversational' (Trigger Chat Bot) or 'Informational' (Trigger RAG Agent)
+- Combines semantic search (vector store) with structured relationship-based search (knowledge graph). 
+- Uses an LLM (language model) to filter and correct results before generating an answer. 
+- Automatically performs a web search if local data isn't enough. 
+- All steps are orchestrated by agents that guide decisions and improve accuracy. 
+
+**Tech Stack:**
+- Python 
+- LangGraph, LangChain (agent and flow management) 
+- Qdrant + Neo4j + Graphiti (vector search + knowledge graph) 
+- FlashRank (smart document re-ranking) 
+- Streamlit (interactive front-end) 
 
 
 ### Knowledge Graph 🎯
@@ -17,7 +35,7 @@ This directory contains code and demo of a self corrective and agentic RAG capab
 </p>
 
 ### Graph Flow
-**Current Version**
+**Current Version:** Added
 
 <img src="./static/graph_flow_v1.png" width="400" style="margin-right:10px;" />
 
--- self-corrective-agentic-RAG/src/graph.py
+++ self-corrective-agentic-RAG/src/graph.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
+import asyncio
 from src import workarounds
 workarounds.monkey_patch()
 
@@ -9,64 +10,64 @@ from langgraph.graph import START, END, StateGraph
 from langgraph.checkpoint.memory import InMemorySaver
 
 from src.nodes import (
-    AgentState, retrieve_from_vectorstore, retrieve_from_knowledge_graph, grade_documents, generate, 
-    rewrite_query, web_search, decide_to_generate
+    AgentState, retrieve_from_all_sources, grade_documents, generate_conversational_response, 
+    generate_rag_response, rewrite_query, web_search, decide_to_generate, classify_query
 )
 
 memory = InMemorySaver()
 
 graph = StateGraph(AgentState)
 
-graph.add_node('retrieve_from_vectorstore', retrieve_from_vectorstore)
-graph.add_node('retrieve_from_knowledge_graph', retrieve_from_knowledge_graph)
+graph.add_node('retrieve_from_all_sources', retrieve_from_all_sources)
 graph.add_node('grade_documents', grade_documents)
-graph.add_node('generate', generate)
+graph.add_node('generate_rag_response', generate_rag_response)
+graph.add_node('generate_conversational_response', generate_conversational_response)
 graph.add_node('transform_query', rewrite_query)
 graph.add_node('web_search', web_search)
 
-graph.add_edge(START, 'retrieve_from_vectorstore')
-graph.add_edge(START, 'retrieve_from_knowledge_graph')
-graph.add_edge('retrieve_from_vectorstore', 'grade_documents')
-graph.add_edge('retrieve_from_knowledge_graph', 'grade_documents')
+graph.add_conditional_edges(
+    source=START,
+    path=classify_query,
+    path_map={
+        'conversational': 'generate_conversational_response',
+        'informational': 'retrieve_from_all_sources'
+    }
+)
+
+graph.add_edge('retrieve_from_all_sources', 'grade_documents')
 graph.add_conditional_edges(
     source='grade_documents',
     path=decide_to_generate,
     path_map={
         'transform_query': 'transform_query',
-        'generate': 'generate'
+        'generate': 'generate_rag_response'
     }
 )
 
 graph.add_edge('transform_query', 'web_search')
-graph.add_edge('web_search', 'generate')
-graph.add_edge('generate', END)
+graph.add_edge('web_search', 'generate_rag_response')
+graph.add_edge('generate_rag_response', END)
+graph.add_edge('generate_conversational_response', END)
 
 agent = graph.compile(checkpointer=memory)
 
 
-def interact_with_agent(
+async def interact_with_agent(
     query: str,
     thread_id: str
 ):
-    
     config = {'configurable': {'thread_id': thread_id}}
-    # response = agent.invoke(
-    #     {
-    #         'question': query
-    #     },
-    #     config=config
-    # )
-    
-    #for debugging -> will be removed once app is built    
-    for output in agent.stream({'question': query}, config=config):
-        for k, v in output.items():
-            print(f'Node: {k}')
-        print('\n---\n')
-        
 
-    print(v['generation'])
-    # return response['generation']
+    response = await agent.ainvoke(
+        {
+            'question': query
+        },
+        config=config
+    )
+    
+
+    return response['generation']
     
     
 if __name__ == '__main__':
-    interact_with_agent(query="Apple Intelligence", thread_id='1')
+    asyncio.run(interact_with_agent(query="Hi my name is Jerry!", thread_id='1'))
