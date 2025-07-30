bb6479d3f0ad2198b5e55627e8a32137583cb165
Chinyelugo Jeremiah <36512525+Jeremyugo@users.noreply.github.com>
2025-07-29 21:13:57 -0600

    refactored code to use Qdrant cloud vector store

--- self-corrective-agentic-RAG/src/vector.py
+++ self-corrective-agentic-RAG/src/vector.py
@@ -1,61 +1,43 @@
 import os
 import sys
+import tempfile
 from pathlib import Path
+
+from dotenv import load_dotenv
+from loguru import logger as log
+
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_compressors import FlashrankRerank
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Qdrant
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain_community.document_compressors import FlashrankRerank
-from utils.config import ENV_FILE_PATH, VECTORSTORE_PATH
-import tempfile
-import shutil
+from utils.config import ENV_FILE_PATH
+from utils.helper_functions import create_qdrant_client
 
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
+
+
 load_dotenv(ENV_FILE_PATH)
 
-from loguru import logger as log
+# Create a Qdrant client
+qdrant_client = create_qdrant_client(
+    url=os.getenv('QDRANT_URL'),
+    api_key=os.getenv('QDRANT_API_KEY'),
+    collection_name='self_corrective_agentic_rag'
+)
+
 
 llm = ChatOpenAI(model='gpt-3.5-turbo')
 
 
-def clear_vectorstore_directory():
-    import stat
-    if os.path.exists(VECTORSTORE_PATH):
-        try:
-            shutil.rmtree(VECTORSTORE_PATH)
-            log.success(f'Cleared directory')
-        except Exception as e:
-            log.error(f'Failed to clear vectorstore directory: {e}')
-            raise
-    try:
-        os.makedirs(VECTORSTORE_PATH, exist_ok=True)
-        os.chmod(VECTORSTORE_PATH, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-    except Exception as e:
-        log.error(f'Failed to create or set permissions for vectorstore directory: {e}')
-        raise
-    return
 
-
-def prepare_data(pdf_files):
-    #clear_vectorstore_directory()
+def prepare_data(pdf_files: list[str]) -> list:
     docs = []
-    # for pdf in pdf_files:
-    #     if hasattr(pdf, 'read'):
-    #         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-    #             tmp_file.write(pdf.read())
-    #             tmp_file_path = tmp_file.name
-    #         loader = PyPDFLoader(tmp_file_path)
-    #         docs.extend(loader.load())
-    #         os.remove(tmp_file_path)
-    #     else:
-    #         loader = PyPDFLoader(pdf)
-    #         docs.extend(loader.load())
+
     for pdf in pdf_files:
         if hasattr(pdf, 'read'):
             pdf_bytes = pdf.read()
@@ -80,27 +62,31 @@ def prepare_data(pdf_files):
         chunk_size=250,
         chunk_overlap=50
     )
-    return splitter.split_documents(docs)
-
-
-def create_vector_database(pdf_files):
+    split_docs = splitter.split_documents(docs)
     
+    return [doc for doc in split_docs if doc.page_content]
+
+
+def create_vector_database(pdf_files: list[str], collection_name: str = 'self_corrective_agentic_rag') -> None:
     doc_splits = prepare_data(pdf_files)
-    log.info(
-        f'Data snippet: {doc_splits[-1].page_content[:50]}...'
-    )
-    Chroma.from_documents(
-        documents=doc_splits,
-        collection_name='rag-chroma',
-        embedding=OpenAIEmbeddings(),
-        persist_directory=VECTORSTORE_PATH
-    )
+    
+    if collection_name not in [c.name for c in client.get_collections().collections]:
+        log.info(f'Creating new collection: {collection_name}')
+        
+        Qdrant.from_documents(
+            api_key=os.getenv('QDRANT_API_KEY'),
+            url=os.getenv('QDRANT_URL'),
+            documents=doc_splits,
+            embedding=OpenAIEmbeddings,
+            collection_name=collection_name,
+        )
+    
     log.success('Vector store created successfully!')
+    
     return
 
 
-
-def add_reranker(vectorstore):
+def add_reranker(vectorstore: Qdrant) -> ContextualCompressionRetriever:
     
     retriever = vectorstore.as_retriever(search_kwargs={'k': 7})
     
@@ -113,39 +99,44 @@ def add_reranker(vectorstore):
     return compression_retriever
 
 
-def add_documents_to_vectorstore(pdf_paths: list[str]):
-    """
-    Adds new PDF documents to an existing Chroma vector store.
-    Args:
-        pdf_paths (list[str]): List of PDF file paths.
-    """
-    vectorstore = Chroma(
-        collection_name='rag-chroma',
-        persist_directory=VECTORSTORE_PATH,
-        embedding_function=OpenAIEmbeddings()
-    )
+def add_documents_to_vectorstore(pdf_paths: list[str], collection_name: str = 'self_corrective_agentic_rag') -> None:
+    
     doc_splits = prepare_data(pdf_paths)
+    
+    vectorstore = Qdrant(
+        client=qdrant_client,
+        collection_name=collection_name,
+        embeddings=OpenAIEmbeddings()
+    )
     vectorstore.add_documents(doc_splits)
-    vectorstore.persist()
+
     log.success('New PDF documents added to vectorstore!')
+    
     return
 
 
-def create_vector_retriever():
-    
-    vectorstore = Chroma(
-        collection_name='rag-chroma',
-        persist_directory=VECTORSTORE_PATH,
-        embedding_function=OpenAIEmbeddings()
+def create_vector_retriever(collection_name: str = 'self_corrective_agentic_rag') -> ContextualCompressionRetriever:
+    vectorstore = Qdrant(
+        client=qdrant_client,
+        collection_name=collection_name,
+        embeddings=OpenAIEmbeddings()
     )
     
+    retriever = vectorstore.as_retriever(
+        search_type='mmr',
+        search_kwargs={'k':7}
+    )
     
-    compression_reranker = add_reranker(vectorstore)
+    compressor = FlashrankRerank(top_n=3)
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor,
+        base_retriever=retriever
+    )
     
-    return compression_reranker
+    return compression_retriever
+
 
 
 if __name__ == '__main__':
-    pdf_paths = ['/home/ubuntu/datascience/Generative-AI-Agents-langChain-langGraph-/self-corrective-agentic-RAG/static/Github Repository Plan.pdf']
+    pdf_paths = ['/home/ubuntu/datascience/Generative-AI-Agents-langChain-langGraph-/self-corrective-agentic-RAG/data/data.pdf']
     create_vector_database(pdf_paths)
-    
